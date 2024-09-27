3816bf6618f2019680f8837eb6771ef017ca402c
nguyengiabao91753 <140630293+nguyengiabao91753@users.noreply.github.com>
2024-09-27 18:50:03 +0700

    edit mapping

--- ProjectSem3/DTOs/DataMapping.cs
+++ ProjectSem3/DTOs/DataMapping.cs
@@ -40,17 +40,11 @@ public class DataMapping : Profile
         CreateMap<BusesTripDTO, BusesTrip>();
 
         CreateMap<Booking, BookingDTO>().ForMember(
-            d => d.BirthDate,
-            s => s.MapFrom(b => b.BirthDate.ToString("dd/MM/yyyy"))
-            ).ForMember(
             d => d.BookingDate,
             s => s.MapFrom(b => b.BookingDate.ToString("HH:mm:ss dd/MM/yyyy"))
             );
 
         CreateMap<BookingDTO, Booking>().ForMember(
-           d => d.BirthDate,
-           s => s.MapFrom(b => DateTime.ParseExact(b.BirthDate, "dd/MM/yyyy", CultureInfo.InvariantCulture))
-           ).ForMember(
            d => d.BookingDate,
            s => s.MapFrom(b => DateTime.ParseExact(b.BookingDate, "HH:mm:ss dd/MM/yyyy", CultureInfo.InvariantCulture))
            );
