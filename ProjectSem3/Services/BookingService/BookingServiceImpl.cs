48c9d6050420e2e9b690ef24ee798fa0dcb270c5
nguyengiabao91753 <140630293+nguyengiabao91753@users.noreply.github.com>
2024-09-27 21:49:40 +0700

    pull

--- ProjectSem3/Services/BookingService/BookingServiceImpl.cs
+++ ProjectSem3/Services/BookingService/BookingServiceImpl.cs
@@ -21,7 +21,7 @@ public class BookingServiceImpl : BookingService
     public string GenerateTicketCode(int plusId)
     {
         var id = "";
-        var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
         var random = new Random();
 
         for (int i = 0; i < 5; i++)
@@ -31,44 +31,58 @@ public class BookingServiceImpl : BookingService
         return id;
     }
 
-    public bool Create(BookingDTO bookingDTO)
+    public bool Create(BookingDTO bookingDTO, List<BookingDetail> bookingDetails)
     {
         try
         {
             var book = mapper.Map<Booking>(bookingDTO);
-            var bustrip = db.BusesTrips.FirstOrDefault(b => b.BusTripId == book.BusTripId);
-            if (bustrip != null)
+            var total = bookingDetails.Sum(d => d.PriceAfterDiscount);
+            book.Total = total;
+            book.BookingDate = DateTime.Now;
+            db.Bookings.Add(book);
+            if (db.SaveChanges() > 0)
             {
-                /*                var seat = db.BusesSeats.FirstOrDefault(s => s.BusId == bustrip.BusId && s.SeatId == book.SeatId);*/
+                var bustrip = db.BusesTrips.SingleOrDefault(b => b.BusTripId == book.BusTripId);
 
-                /*                if (seat != null)
-                                {
-                                    seat.Status = 0;
-                                    db.BusesSeats.Update(seat);
-                                    var countseat = db.BusesSeats.Count(s => s.BusId == bustrip.BusId && s.Status == 1);
-                                    if (countseat == 0)
-                                    {
-                                        bustrip.Status = 2;
-                                        db.BusesTrips.Update(bustrip);
-
-                                    }
-                                    book.BookingDate = DateTime.Now;
-
-                                    db.Bookings.Add(book);
-                                    if (db.SaveChanges() > 0)
-                                    {
-                *//*                        book.TicketCode = GenerateTicketCode(book.BookingId);
-                                        db.Bookings.Update(book);*//*
-                                        return db.SaveChanges() > 0;
-                                    }
+                if (bustrip != null)
+                {
+                    for (int i = 0; i < bookingDetails.Count; i++)
+                    {
 
 
+                        var seat = db.BusesSeats.FirstOrDefault(s => s.BusId == bustrip.BusId && s.SeatId == bookingDetails[i].SeatId);
 
-                                }*/
+                        if (seat != null)
+                        {
+                            seat.Status = 0;
+                            db.BusesSeats.Update(seat);
+                            var countseat = db.BusesSeats.Count(s => s.BusId == bustrip.BusId && s.Status == 1);
+                            if (countseat == 0)
+                            {
+                                bustrip.Status = 2;
+                                db.BusesTrips.Update(bustrip);
 
+                            }
+
+                            bookingDetails[i].BookingId = book.BookingId;
+                            db.BookingDetails.Add(bookingDetails[i]);
+
+                            if (db.SaveChanges() > 0)
+                            {
+                                bookingDetails[i].TicketCode = GenerateTicketCode(bookingDetails[i].BookingDetailId);
+                                db.BookingDetails.Update(bookingDetails[i]);
+                                db.SaveChanges();
+                            }
+
+
+
+                        }
+                    }
+
+                }
             }
 
-            return false;
+                    return false;
 
         }
         catch (Exception ex)
